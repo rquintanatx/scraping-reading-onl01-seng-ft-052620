38b2be3f6b1f75647e7dfd27c0ce5c8f86d949bc
Robert Quintana <robertquintanatx@gmail.com>
2020-06-17 04:39:52 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,5 +4,4 @@ require 'open-uri'
 html = open("https://flatironschool.com/")
 doc = Nokogiri::HTML(html)
 puts doc.css(".title-oE5vT4").text
-
-"id-3c239880-48cf-5ce7-84d0-578546d87586" 
+id-3c239880-48cf-5ce7-84d0-578546d87586
