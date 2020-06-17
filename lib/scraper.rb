c1c24428b4c2c803e45f8f388aaad6d361531e66
Robert Quintana <robertquintanatx@gmail.com>
2020-06-17 04:39:36 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,4 +5,4 @@ html = open("https://flatironschool.com/")
 doc = Nokogiri::HTML(html)
 puts doc.css(".title-oE5vT4").text
 
-<section id="id-3c239880-48cf-5ce7-84d0-578546d87586" aria-label="id-3c239880-48cf-5ce7-84d0-5785
+"id-3c239880-48cf-5ce7-84d0-578546d87586" 
